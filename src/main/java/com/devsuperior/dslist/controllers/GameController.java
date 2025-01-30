6276656d49330e3e8c371ccc6b66c546ddc4de90
Jhonny Willyans <jhonny.aquino@cnj.jus.br>
2025-01-30 14:34:53 -0500

    Pega

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -1,34 +1,34 @@
 package com.devsuperior.dslist.controllers;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import com.devsuperior.dslist.dto.GameDto;
+import com.devsuperior.dslist.dto.GameMinDto;
+import com.devsuperior.dslist.services.GameService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dslist.dto.GameDto;
-import com.devsuperior.dslist.dto.GameMinDto;
-import com.devsuperior.dslist.services.GameService;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/games")
 public class GameController {
-	@Autowired
-	private GameService gameService;
-	
-	@GetMapping
-	public List<GameMinDto> findAll(){
-		List <GameMinDto> result = gameService.findAll();
-		return result;
-		
-	}
-	@GetMapping(value = "/{id}")
-	public GameDto findById(@PathVariable Long id){
-		GameDto result = gameService.findById(id);
-		return result;
-	
-	}
-	
+    private final GameService gameService;
+
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @GetMapping
+    public List<GameMinDto> findAll() {
+        return gameService.findAll();
+
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDto findById(@PathVariable Long id) {
+        return gameService.findById(id);
+
+    }
+
 }
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -1,35 +1,43 @@
 package com.devsuperior.dslist.services;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.devsuperior.dslist.dto.GameDto;
 import com.devsuperior.dslist.dto.GameMinDto;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class GameService {
-		@Autowired
-	private GameRepository gameRepository;
-	
-	
-	
-	@Transactional(readOnly = true)		 
-	public List <GameMinDto> findAll(){
-		 List<Game> result = gameRepository.findAll();
-		 return result.stream().map(x -> new GameMinDto(x)).toList();
-	}
-	
-	@Transactional(readOnly = true)
-	 public GameDto findById(Long id){
-		 Game result = gameRepository.findById(id).get();
-		 GameDto dto = new GameDto(result);
-		 return dto;
-	}
+    @Autowired
+    private GameRepository gameRepository;
+
+
+    @Transactional(readOnly = true)
+    public List<GameMinDto> findAll() {
+        List<Game> result = gameRepository.findAll();
+        return result.stream().map(GameMinDto ::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDto findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+
+        return GameDto.builder()
+                .id(result.getId())
+                .title(result.getTitle())
+                .score(result.getScore())
+                .year(result.getYear())
+                .genre(result.getGenre())
+                .platforms(result.getPlatforms())
+                .imgUrl(result.getImgUrl())
+                .shortDescription(result.getShortDescription())
+                .LongDescription(result.getLongDescription())
+                .build();
+    }
 }
 	
 	
